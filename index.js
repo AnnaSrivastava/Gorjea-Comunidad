56ec423c5b91d68897d30f04be49bc0007ee959c
AnnaSrivastava <ananyavsrivastava@gmail.com>
2020-08-18 17:55:09 +0530

    added public folder

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(3001, function(){
+	console.log("listening at port 3001");
+})
+
+app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+	console.log('made socket connection', socket.id);
+})
