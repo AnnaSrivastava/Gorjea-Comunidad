05a9fff25d3c3ade9a6416b7a36f8a2aaa655ed2
AnnaSrivastava <ananyavsrivastava@gmail.com>
2020-08-20 10:21:09 +0530

    broadcast

--- public/chat.js
+++ public/chat.js
@@ -2,8 +2,11 @@ var socket = io.connect('http://localhost:3001');
 
 var message = document.getElementById('message'),
 	btn = document.getElementById('send'),
-	output = document.getElementById('output');
-	handle = "Anna";
+	output = document.getElementById('output'),
+	handle_1 = document.getElementById('handle'),
+	handle = handle_1.value==='' ? "Anon" : handle_1,
+	feedback = document.getElementById('feedback'),
+	otp = document.getElementById('otp');
 btn.addEventListener('click', function () {
 	socket.emit('chat',{
 		message: message.value,
@@ -12,6 +15,15 @@ btn.addEventListener('click', function () {
 	message.value = "";
 });
 
+message.addEventListener('keypress', function () {
+	socket.emit('typing', handle);
+})
+
 socket.on('chat', function (data) {
+	feedback.innerHTML = '';
 	output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
 });
+
+socket.on('typing', function () {
+	feedback.innerHTML = '<p><em>' + data + 'is typing...</em></p>';
+})
